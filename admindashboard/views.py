7db34dadbbfd26f8d05d93a4285f252358dfcbc9
itsregalo <itsregalo047@gmail.com>
2023-05-11 01:59:43 +0300

    updated UI and tables

--- admindashboard/views.py
+++ admindashboard/views.py
@@ -17,14 +17,16 @@ def dashboard(request):
     total_requests = GarbageCollectionRequest.objects.all().count()
     total_disposals = WasteDisposal.objects.all().count()
     total_pending_request = GarbageCollectionRequest.objects.filter(is_picked=False).count()
-
+    pending_collections = GarbageCollectionRequest.objects.filter(is_picked=False)
     context = {
         'total_clients': total_clients,
         'total_bins': total_bins,
-        'total_collections': total_collections,
+        'total_collections': GarbageCollectionRequest.objects.filter(is_picked=True).count(),
         'total_requests': total_requests,
         'total_disposals': total_disposals,
         'total_users': total_users,
-        'total_pending_request': total_pending_request
+        'total_pending_request': total_pending_request,
+        'pending_collections':pending_collections
+
     }
     return render(request, 'admin-dashboard/admin-dashboard.html', context)
--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from accounts.models import User
+from .models import Client
+from admindashboard.models import *
 
 # Create your views here.
 
 def client_dashboard(request):
-    return render(request, 'clients/client_dashboard.html')
+    try:
+        user_credit_score = CreditScore.objects.get(user=request.user).score
+    except CreditScore.DoesNotExist:
+        user_credit_score = CreditScore.objects.create(user=request.user, score=0)
+    user_garbage_bins = GarbageBin.objects.filter(user=request.user)
+    user_garbage_collections = GarbageCollection.objects.filter(bin__in=user_garbage_bins).count()
+    user_garbage_collection_requests = GarbageCollectionRequest.objects.filter(user=request.user).filter(is_picked=False).count()
+
+    context = {
+        'user_credit_score': user_credit_score,
+        'user_garbage_bins': user_garbage_bins,
+        'user_garbage_collections': user_garbage_collections,
+        'pending_user_garbage_collection_requests': user_garbage_collection_requests
+    }
+
+    return render(request, 'clients/client_dashboard.html', context)
